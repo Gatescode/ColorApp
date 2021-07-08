a38116bcbbee6afa66b94402fa398ec2a704dd47
Arihant Jain <jainarihantmr@gmail.com>
2021-07-08 10:28:23 -0500

    Test 1 for storing new colors in AsyncStorage

--- app/screens/Gallery.js
+++ app/screens/Gallery.js
@@ -58,14 +58,22 @@ async function getCArray(stri){
     });
 }
 
+
+
+
+
+
 export default function Gallery(props){
     const [fontsloaded, setFontsLoaded] = useState(false);
     const [colorsLoaded, setColorsLoaded] = useState(null);
+    const [listCS, setListCS] = useState(null);
     const [list, setList] = useState(variable);
     const curId = list.length > 0 ? list[list.length-1].id +1 : 1;
-    const [currentId, setCurrentId] = useState(curId); 
+    const [currentId, setCurrentId] = useState(curId);
     const [shouldEnterText, setShouldEnterText] = useState(false);
+    const [shouldEnterTextCS, setShouldEnterTextCS] = useState(false);
     const [nameValue, setNameValue] = useState("");
+    const [nameValueCS, setNameValueCS] = useState("");
     const [showPalletteColor, setShowPalletteColor] = useState(null);
     const navigation = useNavigation();
     
@@ -121,17 +129,17 @@ export default function Gallery(props){
                                 </Pressable>
                             ))}
                             
-                            <Pressable style={{backgroundColor: "#8ED1CD",width: "100%", height: (67/734)*windowHeight, borderRadius: 9, alignItems: "center", justifyContent: "center", marginBottom: (80/734)*windowHeight, shadowRadius: 10, shadowOpacity: 0.32, shadowOffset: {width: 2, height: 0}, elevation: 75, display: shouldEnterText ? "none" : "flex"}} onPressOut={() => {setShouldEnterText(true);}}>
+                            <Pressable style={{backgroundColor: "#8ED1CD",width: "100%", height: (67/734)*windowHeight, borderRadius: 9, alignItems: "center", justifyContent: "center", marginBottom: (80/734)*windowHeight, shadowRadius: 10, shadowOpacity: 0.32, shadowOffset: {width: 2, height: 0}, elevation: 10, display: shouldEnterText ? "none" : "flex"}} onPressOut={() => {setShouldEnterText(true);}}>
                                 <Text style={styles.paintingBtnText}>+ Add palette</Text>
                             </Pressable>
                             <View style={{height: (67/734)*windowHeight, display: shouldEnterText ? "flex" : "none",  marginBottom: (80/734)*windowHeight}}>
                                 <TextInput ref={function(input){if(input!=null && !shouldEnterText){input.clear()}}} style={{height: "100%",backgroundColor: '#F5F3F0', borderRadius: 9, fontFamily: "Apercu", fontSize: 25, letterSpacing: -2, paddingLeft: (56/414)*windowWidth}} placeholder={"Type pallette name..."} onChangeText={(val) => setNameValue(val)}>
                                 </TextInput>
-                                <Pressable style={{ backgroundColor: "#8ED1CD", height: (50/734)*windowHeight, width: (50/734)*windowHeight, zIndex: 1, position: "absolute", alignSelf: "flex-end", marginTop: (8.5/734)*windowHeight, right: (8.5/734)*windowHeight, borderRadius: 9, alignItems: "center", justifyContent: "center", shadowRadius: 10, shadowOpacity: 0.32, shadowOffset: {width: 0, height: 2}, elevation: 75}} onPressOut={() => {
+                                <Pressable style={{ backgroundColor: "#8ED1CD", height: (50/734)*windowHeight, width: (50/734)*windowHeight, zIndex: 1, position: "absolute", alignSelf: "flex-end", marginTop: (8.5/734)*windowHeight, right: (8.5/734)*windowHeight, borderRadius: 9, alignItems: "center", justifyContent: "center", shadowRadius: 10, shadowOpacity: 0.32, shadowOffset: {width: 0, height: 2}, elevation: 10}} onPressOut={() => {
                                     if (nameValue.trim()){
                                         setCurrentId(currentId +1); 
 
-                                        const newList = list.concat({ptname: nameValue, id: currentId, hcArray: ["#43175e", "#76ab84"]}); storePaintingNames(newList); console.log("NEWLIST (array)    ------     " + newList); setList(newList);
+                                        const newList = list.concat({ptname: nameValue, id: currentId, hcArray: [/*empty colors for now*/]}); storePaintingNames(newList); console.log("NEWLIST (array)    ------     " + newList); setList(newList);
 
                                         setShouldEnterText(false); setNameValue("");
                                     }
@@ -149,9 +157,12 @@ export default function Gallery(props){
             
             //return (<Colors paintingName={showPalletteColor} hexCodesArray={JSON.stringify(colorsLoaded)}/>);
 
-            
+            /**
+             * 
+             */
             var k = 1;
             var hexCodesArrayReal = JSON.parse(JSON.stringify(colorsLoaded));
+            setListCS(hexCodesArrayReal)
             return (
                 <SafeAreaView style={{top: (45/734)*windowHeight}}>
                     <Pressable style={{position: "absolute", left: (18.2/414)*windowWidth}} onPress={() => {console.log("btn cliked"); setColorsLoaded(null); setShowPalletteColor(null);}}>
@@ -159,18 +170,59 @@ export default function Gallery(props){
                     </Pressable>
                     <Text style={{fontFamily: 'ApercuBold', fontSize: 40, letterSpacing: -2, alignSelf: "center", marginBottom: (27/734)*windowHeight}}>{showPalletteColor}</Text>
                     <ScrollView>
-                        {hexCodesArrayReal.map(item => (
+                        {listCS.map(item => (
                             <View key={++k} style={{backgroundColor: item, width: "100%", justifyContent: 'center', height: (78/734)*windowHeight, paddingLeft: (16/414)*windowWidth}}>
                                 <Text style={{fontFamily: "ApercuBold", fontSize: 25, letterSpacing: -2}}>
                                     {item}
                                 </Text>
                             </View>
                         ))}
+                        
                         <Pressable style={{width: "100%", justifyContent: 'center', alignItems: "center", height: (78/734)*windowHeight}}>
                             <Text style={{fontFamily: "ApercuBold", fontSize: 25, letterSpacing: -2}}>
                                 + Add color
                             </Text>
                         </Pressable>
+                        <View style={{height: (67/734)*windowHeight, display: shouldEnterTextCS ? "flex" : "none",  marginBottom: (80/734)*windowHeight}}>
+                            <TextInput ref={function(input2){if(input2!=null && !shouldEnterTextCS){input2.clear()}}} style={{height: "100%",backgroundColor: '#F5F3F0', borderRadius: 9, fontFamily: "Apercu", fontSize: 25, letterSpacing: -2, paddingLeft: (56/414)*windowWidth}} placeholder={"Type pallette name..."} onChangeText={(val) => setNameValueCS(val)}>
+                            </TextInput>
+                            <Pressable style={{ backgroundColor: "#8ED1CD", height: (50/734)*windowHeight, width: (50/734)*windowHeight, zIndex: 1, position: "absolute", alignSelf: "flex-end", marginTop: (8.5/734)*windowHeight, right: (8.5/734)*windowHeight, borderRadius: 9, alignItems: "center", justifyContent: "center", shadowRadius: 10, shadowOpacity: 0.32, shadowOffset: {width: 0, height: 2}, elevation: 10}} onPress={() => {
+                                if (nameValueCS.trim()){
+                                    
+                                    //WE ALREADY HAVE THE ARRAY AT @listCS
+                                    const newListCS = listCS.concat(nameValueCS);
+                                    setListCS(newListCS);
+                                    //storePaintingNames({ptname: nameValue, id: currentId, hcArray: [/*empty colors for now*/]})
+
+                                    //get current paintingnames, set current paitning name color array to newListCS
+
+                                    try {
+                                        AsyncStorage.getItem("paintingnames").then((biglist) => {
+                                            if (biglist === null){
+                                                biglist = [];
+                                            }
+                                            var vartest = JSON.parse(biglist);
+                                            for (var i =0;i<vartest.length;i++){
+                                                if (vartest[i].ptname == showPalletteColor){
+                                                    vartest[i].hcArray = newListCS;
+                                                    AsyncStorage.setItem("paintingnames", JSON.stringify(vartest));
+                                                    break;
+                                                }
+                                            }
+                                        });
+                        
+                                    } catch (er) {
+                                        console.error(er);
+                                    }
+
+                                    setShouldEnterTextCS(false); setNameValueCS("");
+                                }
+                            }}>
+                                
+                                <Text style={{fontFamily: "ApercuBold", fontSize: 35}}>+</Text>
+
+                            </Pressable>
+                        </View>
                     </ScrollView>
                 </SafeAreaView>
             );
